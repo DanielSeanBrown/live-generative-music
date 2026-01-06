ba73e89473ae4817b2d0f47a44e1929f436a8689
DanielSeanBrown <danielseanbrown@gmail.com>
2026-01-06 19:57:52 +0000

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+'''
+src package fpr my real-time multimodal music and lyric generator.
+Contains modules for:
+- data fetching/preprocessing
+- music model
+- lyric model
+- real-time control
+'''
+__version__ = '0.0.0'
